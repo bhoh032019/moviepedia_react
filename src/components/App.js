e05b69b7376d3afc75dea5646ea808188bb5bf04
bhoh032019 <bhoh0320@ajou.ac.kr>
2024-10-02 12:49:36 +0900

    feat: 네트워크 관련 에러 처리 기능 추가

--- src/components/App.js
+++ src/components/App.js
@@ -10,6 +10,8 @@ function App() {
   const sortedItems = items.sort((a, b) => b[order] - a[order]);
   const [offset, setOffset] = useState(0);
   const [hasNext, setHasNext] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [loadingError, setLoadingError] = useState(null);
 
   const handleNewestClick = () => setOrder('createdAt');
   const handleBestClick = () => setOrder('rating');
@@ -20,11 +22,23 @@ function App() {
   };
 
   const handleLoad = async (options) => {
-    const { reviews, paging } = await getReviews(options);
+    let result;
+    try {
+      setIsLoading(true);
+      setLoadingError(null);
+      result = await getReviews(options);
+    } catch (error) {
+      setLoadingError(error);
+      console.error(error);
+      return;
+    } finally {
+      setIsLoading(false);
+    }
+    const { reviews, paging } = result;
     if (options.offset === 0) {
       setItems(reviews);
     } else {
-      setItems([...items, ...reviews]);
+      setItems((prevItems) => [...prevItems, ...reviews]);
     }
     setOffset(options.offset + reviews.length);
     setHasNext(paging.hasNext);
@@ -45,7 +59,12 @@ function App() {
         <button onClick={handleBestClick}>베스트순</button>
       </div>
       <ReviewList items={sortedItems} onDelete={handleDelete} />
-      {hasNext && <button onClick={handleLoadMore}>더 보기</button>}
+      {hasNext && (
+        <button disabled={isLoading} onClick={handleLoadMore}>
+          더 보기
+        </button>
+      )}
+      {loadingError?.message && <span>{loadingError.message}</span>}
     </div>
   );
 }
